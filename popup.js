6904b167937d4d6862dd6b83b5954728e6a51f0e
Kevin <ukd210@gmail.com>
2023-11-09 07:28:09 +0900

    first modified

--- popup.js
+++ popup.js
@@ -4,29 +4,16 @@
 * ---------------------------------------------------------------------------------
 **/
 
-// changeColor ID element 를 취득
-let changeColor = document.getElementById("changeColor");
+document.addEventListener("DOMContentLoaded", function() {
+  const urlInput = document.getElementById("urlInput");
+  const openButton = document.getElementById("openButton");
 
-// 스토리지에 저장되어 있는 컬러가 있다면 표시
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
-});
-
-// 배경색 버튼을 클릭하였을 경우 이벤트 등록
-changeColor.addEventListener("click", async () => {
-  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+  openButton.addEventListener("click", function() {
+    // https://e2.me/metamemes
+    // https://app.earth2.io/#thegrid/9f6679c9-1908-4e26-bdff-9acaf14446d3
+    const url = `https://e2.me/${urlInput.value}`;
+    if (url) {
+      chrome.tabs.create({ url });
+    }
   });
 });
-
-/**
- * @description 현재 웹 페이지의 Body 요소의 배경색을 변경해주는 함수
- **/
-function setPageBackgroundColor() {
-  chrome.storage.sync.get("color", ({ color }) => {
-    document.body.style.backgroundColor = color;
-  });
-}
