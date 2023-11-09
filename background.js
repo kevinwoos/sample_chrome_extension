e4f15512e7e1f523adf3320171f8210898de8d80
Kevin <ukd210@gmail.com>
2023-11-09 15:35:38 +0900

    기능별로 디렉토리 분리

--- background.js
+++ background.js
@@ -8,5 +8,4 @@ let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
-  console.log('기본 배경색은 %cgreen', `color: ${color}`);
 });
--- js/popup.js
+++ js/popup.js
@@ -9,9 +9,6 @@ document.addEventListener("DOMContentLoaded", function() {
   const openButton = document.getElementById("openButton");
 
   openButton.addEventListener("click", function() {
-    // https://e2.me/metamemes
-    // https://app.earth2.io/#thegrid/9f6679c9-1908-4e26-bdff-9acaf14446d3
-    // const url = `https://e2.me/${urlInput.value}`;
     const url = `https://app.earth2.io/#propertyInfo/${urlInput.value}`;
     if (url) {
       chrome.tabs.create({ url });
