18813fd7c4e9e8487b42fee851eca2acec9eb817
Kevin <ukd210@gmail.com>
2023-11-09 13:13:52 +0900

    popup 관련 수정

--- popup.js
+++ popup.js
@@ -11,7 +11,8 @@ document.addEventListener("DOMContentLoaded", function() {
   openButton.addEventListener("click", function() {
     // https://e2.me/metamemes
     // https://app.earth2.io/#thegrid/9f6679c9-1908-4e26-bdff-9acaf14446d3
-    const url = `https://e2.me/${urlInput.value}`;
+    // const url = `https://e2.me/${urlInput.value}`;
+    const url = `https://app.earth2.io/#propertyInfo/${urlInput.value}`;
     if (url) {
       chrome.tabs.create({ url });
     }
